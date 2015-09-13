205c33d1147c24395757fd68d6f93fa8b5232134
Benjamin Borowski <ben.borowski@typeoneerror.com>
2015-09-13 11:48:50 -0700

    Example: display a string

--- content-for/ember-cli-build.js
+++ content-for/ember-cli-build.js
@@ -3,7 +3,11 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    inlineContent: {
+      'string-content': {
+        content: '<!-- HELLO WORLD -->'
+      }
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
